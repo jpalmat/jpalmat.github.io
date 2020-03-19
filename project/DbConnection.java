299d3b2d1b6fe0ea0793384ce6c6c3bc603ab0c5
jpalmatcls <jimmy.palma1990@gmail.com>
2020-03-19 13:08:29 -0500

    change comment

--- project/DbConnection.java
+++ project/DbConnection.java
@@ -13,14 +13,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author jimmypalma
